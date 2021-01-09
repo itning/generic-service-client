0b1304c2521cde4ec78481fe276863e25917a5b6
itning <ning.wang8@geely.com>
2021-01-09 20:19:12 +0800

    feat: web

--- generic-service-web/src/main/java/top/itning/generic/service/web/WebController.java
+++ generic-service-web/src/main/java/top/itning/generic/service/web/WebController.java
@@ -1,5 +1,26 @@
-package top.itning.generic.service.web;/**
+package top.itning.generic.service.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
  * @author itning
  * @since 2021/1/9 20:10
- */public class WebController {
+ */
+@Controller
+public class WebController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/generic")
+    public String index2() {
+        return "index";
+    }
+
+    @GetMapping("/index.html")
+    public String index3() {
+        return "index";
+    }
 }
